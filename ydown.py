19e3d5701b0761ba12d9f95d985c1b240ca63a09
SnChul <104672461+SnChul@users.noreply.github.com>
2022-05-10 23:27:29 +0900

    [update] mp3 체크기능 생성 경로지정 추후 수정

--- ydown.py
+++ ydown.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import filedialog
 from pytube import YouTube
 import shutil
-
+import os
+from moviepy.editor import VideoFileClip
 root=Tk()
 root.title("Youtube Downloader")
 root.geometry('300x200+1100+500')
@@ -11,18 +12,31 @@ def select_path():
     path1 = filedialog.askdirectory()
     path_label.insert(0, path1)
     
-def download():
+def fdownload():
     ytb_url = url_label.get()
     where = path_label.cget("text")
     ytb_vd = YouTube(ytb_url)
-    ytb_vd = YouTube(ytb_url).streams.get_highest_resolution().download()
-    shutil(ytb_vd, where)
-    
+    ytb_vd = YouTube(ytb_url).streams.filter(only_audio=True).first()
+    ytb_vd.download()
+    if chk.get() == 1:
+     name = ytb_vd.download()
+     front, back = os.path.splitext(name)
+     rename = front + '.mp3'
+     os.rename(name, rename)
+
+
+
+
+chk = IntVar()
+checkbox = Checkbutton(root, text="MP3", variable=chk)    
 path_label = Entry(root, width=28)
 path_btn1 = Button(root, text="Path", command=select_path)
 url_label = Entry(root, width=28)
-download_btn1 = Button(root, text="Download",command=download )
+download_btn1 = Button(root, text="Download",command=fdownload )
 
+
+
+checkbox.grid(row=1, column=1)
 url_label.grid(row=1, column=0)
 path_label.grid(row=2, column=0)
 path_btn1.grid(row=2, column=1)
